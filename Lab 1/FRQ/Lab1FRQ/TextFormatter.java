86746bf7aa8d78b58f9e4d3146c2095ffa394215
Pranav Mathur <pranavmathur99@gmail.com>
2015-02-14 20:26:25 -0800

    Sync

---
+++ Lab 1/FRQ/Lab1FRQ/TextFormatter.java
@@ -0,0 +1,23 @@
+public class TextFormatter {
+   
+   private String line;
+   
+   public TextFormatter(String lineToFormat) {
+      line = lineToFormat;
+   }
+   
+   public int findString(String str, int start) {
+      int i = start;
+      if (i == 0 && line.subString(i, i+1).equals(str) &&
+            !line.substring
+   }   
+   
+   public int countStrings(String str) {
+      return 0;
+   }
+   
+   public String convertItalics() {
+      return null;
+   }      
+   
+}
